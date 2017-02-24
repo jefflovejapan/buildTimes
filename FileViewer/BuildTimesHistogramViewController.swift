68fd321f7e2321caf195480fb7bdfac5a53a7b24
Jeffrey Blagdon <jeffblagdon@gmail.com>
2017-02-24 15:18:51 -0500

    Resizing window works

--- FileViewer/BuildTimesHistogramViewController.swift
+++ FileViewer/BuildTimesHistogramViewController.swift
@@ -9,6 +9,17 @@
 import AppKit
 
 class HistogramBar: NSCollectionViewItem {
+  weak var mouseDelegate: MouseDelegate?
+
+  override func mouseEntered(with event: NSEvent) {
+    mouseDelegate?.mousedOver(sender: self)
+  }
+
+  override func mouseDown(with event: NSEvent) {
+    if event.clickCount > 1 {
+      mouseDelegate?.doubleClicked(sender: self)
+    }
+  }
 }
 
 extension CGColor {
@@ -20,7 +31,18 @@ extension CGColor {
   }
 }
 
-class BuildTimesHistogramViewController: NSViewController, NSCollectionViewDelegate, NSCollectionViewDataSource {
+protocol MouseDelegate: NSObjectProtocol {
+  func doubleClicked(sender: NSCollectionViewItem)
+  func mousedOver(sender: NSCollectionViewItem) 
+}
+
+extension NSViewController {
+  var window: NSWindow? {
+    return NSApplication.shared().keyWindow
+  }
+}
+
+class BuildTimesHistogramViewController: NSViewController, NSCollectionViewDelegate, NSCollectionViewDataSource, MouseDelegate, NSCollectionViewDelegateFlowLayout {
   
   @IBOutlet weak var collectionView: NSCollectionView!
 
@@ -33,15 +55,51 @@ class BuildTimesHistogramViewController: NSViewController, NSCollectionViewDeleg
 
   var selectedBuildTimes: [FunctionBuildTime] = []
 
+  static private let baseItemSize: NSSize = NSSize(width: 60, height: 500)
+  static private let baseSeparatorWidth: CGFloat = 30
+
+  var itemSize: NSSize = BuildTimesHistogramViewController.baseItemSize {
+    didSet {
+      updateCollectionView(itemSize: itemSize)
+    }
+  }
+
+  private func updateCollectionView(itemSize: NSSize) {
+    collectionView?.reloadData()
+  }
+
+  var itemScale: CGFloat = 1 {
+    didSet {
+      if let windowSize = window?.contentView?.bounds.size {
+        itemSize = itemSize(windowSize: windowSize, itemScale: itemScale)
+      }
+    }
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     let nib = NSNib(nibNamed: String(describing: HistogramBar.self), bundle: nil)
     collectionView.register(nib, forItemWithIdentifier: String(describing: HistogramBar.self))
+    NotificationCenter.default.addObserver(self, selector: #selector(windowResized), name: Notification.Name.NSWindowDidResize, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(windowResized), name: Notification.Name.NSWindowDidEndLiveResize, object: nil)
+  }
+
+  @objc private func windowResized() {
+    if let windowSize = window?.contentView?.bounds.size {
+      itemSize = itemSize(windowSize: windowSize, itemScale: itemScale)
+    }
+  }
+
+  private func itemSize(windowSize: NSSize, itemScale: CGFloat) -> NSSize {
+    return NSSize(width: BuildTimesHistogramViewController.baseItemSize.width * itemScale, height: windowSize.height)
   }
 
   override func viewDidAppear() {
     super.viewDidAppear()
     collectionView.reloadData()
+    if let windowSize = window?.contentView?.bounds.size {
+      itemSize = itemSize(windowSize: windowSize, itemScale: itemScale)
+    }
   }
 
   func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -53,6 +111,22 @@ class BuildTimesHistogramViewController: NSViewController, NSCollectionViewDeleg
       fatalError()
     }
     histogramBar.view.layer?.backgroundColor = .random
+    histogramBar.mouseDelegate = self
     return histogramBar
   }
+
+
+
+  func doubleClicked(sender: NSCollectionViewItem) {
+    print("double clicked: \(sender)")
+  }
+
+  func mousedOver(sender: NSCollectionViewItem) {
+    print("moused over: \(sender)")
+  }
+
+  func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> NSSize {
+    return itemSize
+  }
+
 }
--- FileViewer/WindowController.swift
+++ FileViewer/WindowController.swift
@@ -39,4 +39,6 @@ class WindowController: NSWindowController {
       self.contentViewController?.representedObject = openPanel.url
     }
   }
+
+  
 }
